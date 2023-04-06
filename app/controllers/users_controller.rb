1168792127f1943742550163a1a703e1f99ad1b7
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-05 17:23:47 -0700

    Refactor login/register features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash[:error] = @user.errors.full_messages.first
       redirect_to new_user_path
     end 
   end
