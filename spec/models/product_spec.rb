05912d8342a414c86cf46bed9d8b246135a9c16b
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-10 13:24:34 -0700

    Refator product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,43 +6,50 @@ RSpec.describe Product, type: :model do
       @category = Category.create(name: 'Evergreens')
     end
 
-    describe '#name' do
+    describe 'name' do
       it 'validates presence of name' do
-        @product = Product.new(name: nil)
-        @product.validate
-        expect(@product.errors.full_messages).to include("Name can't be blank")
+        product = Product.new(name: nil)
+        product.validate
+        expect(product.errors.full_messages).to include("Name can't be blank")
       end
     end
 
-    describe '#price' do
+    describe 'price' do
       it 'validates presence of price' do
-        @product = Product.new(price_cents: nil)
-        @product.validate
-        expect(@product.errors.full_messages).to include("Price can't be blank")
+        product = Product.new(price_cents: nil)
+        product.validate
+        expect(product.errors.full_messages).to include("Price can't be blank")
       end
     end
 
-    describe '#quantity' do
+    describe 'quantity' do
       it 'validates presence of quantity' do
-        @product = Product.new(quantity: nil)
-        @product.validate
-        expect(@product.errors.full_messages).to include("Quantity can't be blank")
+        product = Product.new(quantity: nil)
+        product.validate
+        expect(product.errors.full_messages).to include("Quantity can't be blank")
       end
     end
 
-    describe '#category' do
+    describe 'category' do
       it 'validates presence of category' do
-        @product = Product.new(category: nil)
-        @product.validate
-        expect(@product.errors.full_messages).to include("Category can't be blank")
+        product = Product.new(category: nil)
+        product.validate
+        expect(product.errors.full_messages).to include("Category can't be blank")
       end
     end
 
-    describe '#fields' do
+    describe 'all fields' do
       it 'validates presence of all fields' do
-        @product = Product.new(name: 'Giant Tea', price_cents: 64.99, quantity: 0, category: @category)
-        @product.validate
-        expect(@product.errors.full_messages).to be_empty
+        product = Product.new(name: 'Giant Tea', price_cents: 64.99, quantity: 0, category: @category)
+        product.validate
+        expect(product.errors.full_messages).to be_empty
+
+        product.name = nil
+        product.price_cents = nil
+        product.quantity = nil
+        product.category = nil
+        product.validate
+        expect(product.errors.full_messages).to include("Name can't be blank", "Price cents is not a number", "Quantity can't be blank", "Category can't be blank")
       end
     end
   end
